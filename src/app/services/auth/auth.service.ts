2020946cf5e01e60d105ffdc78215a29125995b4
omar karake <omar.karake@amalitechtraining.org>
2024-09-30 12:40:04 +0200

    chore: removing unnecessary comments in auth service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -45,26 +45,9 @@ export class AuthService {
   }
 
   private loggedIn = false;
-  private users = [
-    { email: 'omar@gmail.com', password: 'Omar12345' }, // Static user
-  ];
 
   // Login method
   login(email: string, password: string): Observable<any> {
-    // Check if user credentials match the static user
-    const user = this.users.find(
-      (user) => user.email === email && user.password === password
-    );
-
-    if (user) {
-      this.loggedIn = true;
-      return of({ success: true });
-    } else {
-      return of({ success: false });
-    }
-
-    // ------------------------------
-
     return this.signin(email, password).pipe(
       take(1), // Ensures the observable completes after one emission
       // Handle success and error cases within the pipe
@@ -80,17 +63,6 @@ export class AuthService {
 
   // Signup method
   signup(email: string, password: string): Observable<any> {
-    // Simulate storing user data in the array
-    // const userExists = this.users.find((user) => user.email === email);
-
-    // if (!userExists) {
-    //   this.users.push({ email, password });
-    //   return of({ success: true });
-    // } else {
-    //   return of({ success: false, message: 'User already exists' });
-    // }
-
-    // ------------------------------
     const username = email.split('@')[0];
 
     return this.register(email, username, password).pipe(
